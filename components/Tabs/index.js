67e0777f265c1f75903f46b04f037987c89004fb
XAmi101 <famousvacancy@gmail.com>
2019-08-09 15:36:53 -0400

    fixed tabs JS. MVP complete

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -13,34 +13,37 @@ let topicTabs = document.querySelector('.topics');
 axios.get('https://lambda-times-backend.herokuapp.com/topics')
   .then((response) => {
     response.data.topics.forEach(items => {
-      let newTabs = createTab(items)
+      let newTabs = document.createElement('div');
+      newTabs.classList.add('tab');
+      newTabs.textContent = items;
       topicTabs.appendChild(newTabs);
+
     })
   })
   .catch((error) => {
     console.log(error);
   })
 
-function createTab(topicsList) {
-  const freshTab = document.createElement('div');
-  freshtab.classList.add('tab');
-  freshtab.textContent = topic;
-  
-  tab.addEventListener('click', () => {
-      let card = document.querySelectorAll('.card');
-      card.forEach((cards) => {
-          cards.style.display = 'none'
-          if (cards.dataset.tab == tab.dataset.tab) {
-              cards.style.display = 'flex'
-          }
-      })
-      let allTabs = document.querySelectorAll('.tab');
-      allTabs.forEach((freshTab) => {
-          freshTab.classList.remove('active-tab');
-      })
-      tab.classList.add('active-tab');
-  })
-
-  return freshtab;
-
-}
+// function createTab(topicsList) {
+//   const freshTab = document.createElement('div');
+//   freshtab.classList.add('tab');
+//   freshtab.textContent = topic;
+//
+//   tab.addEventListener('click', () => {
+//       let card = document.querySelectorAll('.card');
+//       card.forEach((cards) => {
+//           cards.style.display = 'none'
+//           if (cards.dataset.tab == tab.dataset.tab) {
+//               cards.style.display = 'flex'
+//           }
+//       })
+//       let allTabs = document.querySelectorAll('.tab');
+//       allTabs.forEach((freshTab) => {
+//           freshTab.classList.remove('active-tab');
+//       })
+//       tab.classList.add('active-tab');
+//   })
+//
+//   return freshtab;
+//
+// }
